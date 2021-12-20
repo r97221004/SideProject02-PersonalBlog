b9dac0b420fefb85077dfcfcc748b6b1359faf7c
r97221004 <r97221004@ntu.edu.tw>
2021-12-20 13:47:53 +0800

    更新 wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,6 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from PersonalBlog import app
+from PersonalBlog import create_app
+
+app = create_app('production')
